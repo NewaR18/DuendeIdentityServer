670764ddb73cece154b3b56dc714e454637a0a2d
sandip.shrestha <sudip.shrestha@CBS-SUDIP>
2024-07-18 13:17:24 +0545

    EFCore Integrated with SQL Server | Task #5

--- DuendeIdentityServer/Program.cs
+++ DuendeIdentityServer/Program.cs
@@ -1,17 +1,35 @@
 using IdentityServer;
+using IdentityServer.Utilities;
+using IdentityServerHost;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+//builder.Services.AddIdentityServer()
+//                    .AddInMemoryClients(Config.Clients)
+//                    .AddInMemoryIdentityResources(Config.IdentityResources)
+//                    //.AddInMemoryApiResources(Config.ApiResources)
+//                    .AddInMemoryApiScopes(Config.ApiScopes)
+//                    .AddTestUsers(Config.TestUsers)
+//                    .AddDeveloperSigningCredential();
+var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+var connString = builder.Configuration.GetConnectionString("IdentityConnection");
+
 builder.Services.AddIdentityServer()
-                    .AddInMemoryClients(Config.Clients)
-                    .AddInMemoryIdentityResources(Config.IdentityResources)
-                    //.AddInMemoryApiResources(Config.ApiResources)
-                    .AddInMemoryApiScopes(Config.ApiScopes)
-                    .AddTestUsers(Config.TestUsers)
-                    .AddDeveloperSigningCredential();
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connString,
+            sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b => b.UseSqlServer(connString,
+            sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
+    .AddTestUsers(Config.TestUsers);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +46,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
-
+app.InitializeDatabase();
 app.MapRazorPages().RequireAuthorization();
 app.MapControllerRoute(
     name: "default",
